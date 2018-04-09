f627fb98933f4094207456f2bd69437df3b2c711
Ересковский Антон Алексеевич <antoner2@gmail.com>
2018-04-09 22:39:24 +0300

    j

---
+++ AlphatronMarineServer/Controllers/APIController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using AlphatronMarineServer.Models;
+
+namespace AlphatronMarineServer.Controllers
+{
+    public class APIController : Controller
+    {
+        public string Vessels()
+        {
+            return ApiModel.GetVesselsList();
+        }
+        public string Equipments(int id)
+        {
+            return ApiModel.GetEquipmentByIMO(id);
+        }
+    }
+}
